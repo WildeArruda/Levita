e6d09b3c44c7600540fbcf8609fc5939c153a06b
WildeArruda <wildearruda@gmail.com>
2014-10-09 12:03:26 -0300

    Add FacesContextUtil

---
+++ Levita/src/main/java/br/com/ambiciousteam/levita/util/FacesContextUtil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ambiciousteam.levita.util;
+
+import javax.faces.context.FacesContext;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Wilde Arruda <wildearruda@gmail.com>
+ */
+public class FacesContextUtil {
+
+    public static final String HIBERNATE_SESSION = "hibernate_session";
+    
+    public static void setRequestSession(Session session) {
+    
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
+        
+    }
+
+    public static Session getHIBERNATE_SESSION() {
+        return (Session)FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
+    }
+    
+    
+    
+}
